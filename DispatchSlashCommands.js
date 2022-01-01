4ac99f6a0c2e5e423cd9f1ff1b3305bc240b2cd2
Haxrox <56497846+Haxrox@users.noreply.github.com>
2021-12-31 20:34:52 -0800

    edited dispatch script

--- DispatchSlashCommands.js
+++ DispatchSlashCommands.js
@@ -1,31 +1,79 @@
-const DiscordBuilders = require("@discordjs/builders");
+const {SlashCommandBuilder} = require("@discordjs/builders");
+const {REST} = require("@discordjs/rest");
+const {Routes} = require("discord-api-types/v9");
+const FileSystem = require('fs');
 const Configuration = require('./config.json');
-const RestLibrary = require("@discordjs/rest");
-const DiscordAPiTypes = require("discord-api-types/v9");
-const SlashCommandBuilder = DiscordBuilders.SlashCommandBuilder;
 
-const Rest = new RestLibrary.REST({version: '9'}).setToken(Configuration.TOKEN);
+const DISPATCH_GUILD_COMMANDS = false;
+const DELETE_COMMANDS = false;
+const DISPATCH_COMMANDS = true;
 
-const Commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-].map(command => command.toJSON());
+const rest = new REST({version: '9'}).setToken(Configuration.TOKEN);
+const commands = [];
 
-Rest.put(DiscordAPiTypes.Routes.applicationCommands(Configuration.CLIENT_ID), {
-    body: Commands
-}).then((data) => {
-    console.log("Registered application commands");
+if (DISPATCH_COMMANDS) {
+    FileSystem.readdirSync('./Commands').filter(file => (file.endsWith('.js') && file != 'Command.js' && file != 'SlashCommand.js')).forEach(file => {
+        const command = require(`./Commands/${file}`);
+        commands.push(command.GetData());
+    });
+} 
+/*
+else {
+    commands.push(
+        new SlashCommandBuilder().setName('ping').setDescription('Replies with the'),
+        new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+        new SlashCommandBuilder().setName('user').setDescription('Replies with user info!')
+    )
+}
+*/
 
-    /* Deleting slash commands
-    const promises = [];
-    for (const command of data) {
-        const deleteUrl = `${DiscordAPiTypes.Routes.applicationCommands(Configuration.CLIENT_ID)}/${command.id}`;
-        promises.push(Rest.delete(deleteUrl));
-    }
-    return Promise.all(promises);
-    */
-}).catch(console.error);
+if (DISPATCH_GUILD_COMMANDS) {
+    rest.put(Routes.applicationGuildCommands(Configuration.CLIENT_ID, Configuration.GUILD_ID), {
+        body: DISPATCH_COMMANDS ? commands.map(command => command.toJSON()) : []
+    }).then((data) => {
+        console.log("Registered guild commands [%d]:", data.length);
+        data.forEach(command => {
+            console.log(`Registered %s [${command.id}]: %s`, command.name, command.description);
+        });
+    
+        if (DELETE_COMMANDS) {
+            const promises = [];
+            data.filter(command => true).forEach(command => {
+                console.log(`Deleting %s [${command.id}]: %s`, command.name, command.description);
+                const deleteUrl = `${Routes.applicationGuildCommands(Configuration.CLIENT_ID, Configuration.GUILD_ID)}/${command.id}`;
+                promises.push(rest.delete(deleteUrl));
+            });
+            return Promise.all(promises);
+        }
+    })
+    .catch(console.error)
+    .finally(() => {
+        console.log("Dispatched guild commands");
+    });
+} else {
+    rest.put(Routes.applicationCommands(Configuration.CLIENT_ID), {
+        body: DISPATCH_COMMANDS ? commands.map(command => command.toJSON()) : []
+    }).then((data) => {
+        console.log("Registered commands [%d]:", data.length);
+        data.forEach(command => {
+            console.log(`Registered %s [${command.id}]: %s`, command.name, command.description);
+        });
+
+        if (DELETE_COMMANDS) {
+            const promises = [];
+            data.filter(command => true).forEach(command => {
+                console.log(`Deleting %s [${command.id}]: %s`, command.name, command.description);
+                const deleteUrl = `${Routes.applicationGuildCommands(Configuration.CLIENT_ID)}/${command.id}`;
+                promises.push(rest.delete(deleteUrl));
+            });
+            return Promise.all(promises);
+        }
+    })
+    .catch(console.error)
+    .finally(() => {
+        console.log("Dispatched client commands");
+    });
+}
 
 
 
