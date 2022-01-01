3a1b0d0337097cd3772aa5487b1c4ebf7fdd1521
Haxrox <56497846+Haxrox@users.noreply.github.com>
2021-12-31 20:35:59 -0800

    embed reply for Ping command

--- commands/Ping.js
+++ commands/Ping.js
@@ -1,9 +1,17 @@
 const Command = require('./Command.js');
+const {MessageEmbed} = require('discord.js');
 
 class Ping extends Command {
     async Execute(interaction) {
-        super.Execute();
-        await interaction.reply(`${interaction.client.ws.ping}ms`);
+        const embed = new MessageEmbed()
+            .setAuthor({name: interaction.client.user.username, iconURL: interaction.client.user.avatarURL()})
+            .setTitle(`${interaction.client.user.username} Ping`)
+            .setDescription(`${interaction.client.ws.ping}ms`)
+            .setColor('#cacaca')
+            .setTimestamp()
+            .setFooter(`Requested by: ${interaction.user.username}`, interaction.user.avatarURL());
+            // .setFooter({name: `Requested by: ${interaction.user.username}`, iconURL: interaction.user.avatarURL()});
+        await interaction.reply({embeds: [embed]});
     }
 }
 
