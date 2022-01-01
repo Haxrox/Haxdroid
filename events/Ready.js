fca27f5dd1569f1518e2b98ba7b4b760eaf31776
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-01-01 14:34:04 -0800

    added bot presence status

--- events/Ready.js
+++ events/Ready.js
@@ -1,7 +1,10 @@
 const ClientEvent = require("./ClientEvent.js");
 
 class Ready extends ClientEvent {
-    
+    Execute(client) {
+        super.Execute(client);
+        client.user.setPresence({activities: [{name: "Hello World!", type: "PLAYING"}], status: "online"});
+    }
 }
 
 module.exports = new Ready("ready", true);
