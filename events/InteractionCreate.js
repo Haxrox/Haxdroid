2214c77372e264187e7885d788bc504ae8a6f4c2
Haxrox <56497846+Haxrox@users.noreply.github.com>
2021-12-31 20:36:31 -0800

    made casing consistent between cache and command execution

--- events/InteractionCreate.js
+++ events/InteractionCreate.js
@@ -5,14 +5,14 @@ const ClientEvent = require("./ClientEvent.js");
 const slashCommands = new Discord.Collection();
 FileSystem.readdirSync('./Commands').filter(file => (file.endsWith('.js') && file != 'Command.js' && file != 'SlashCommand.js')).forEach(file => {
     const command = require(`../Commands/${file}`);
-    slashCommands.set(command.commandName, command);
+    slashCommands.set(command.commandName.toUpperCase(), command);
 });
 
 class InteractionCreate extends ClientEvent {
     async Execute(client, interaction) {
         super.Execute(client, interaction);
         if (interaction.isCommand()) {
-            const command = slashCommands.get(interaction.commandName);
+            const command = slashCommands.get(interaction.commandName.toUpperCase());
             if (command) {
                 await command.Execute(interaction);
             } else {
