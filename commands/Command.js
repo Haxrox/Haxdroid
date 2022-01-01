5b0aabf9a3787bc41445b6163d118871ee8dba9a
Haxrox <56497846+Haxrox@users.noreply.github.com>
2021-12-31 20:53:04 -0800

    added specifications for command class

--- commands/Command.js
+++ commands/Command.js
@@ -2,20 +2,38 @@ const DiscordBuilders = require("@discordjs/builders");
 const {SlashCommandBuilder} = DiscordBuilders;
 
 module.exports = class Command {
+    /**
+     * Abstract class for commands executed by the user
+     * @abstract
+     * @param {String} name name of the command
+     * @param {String} description description of the command
+     */
     constructor(name, description) {
         this.commandName = name;
         this.description = description;
         this.data = new SlashCommandBuilder().setName(name).setDescription(description);
     }
 
+    /**
+     * Gets the SlashCommandBuilder used to create this command
+     * @returns a SlashCommandBuilder for this command
+     */
     GetData() {
         return this.data;
     }
 
+    /**
+     * Sets the permissions necessary to execute the command
+     * @param {Array} permissions a permissions array
+     */
     SetPermissions(permissions) {
         this.Permissions = permissions;
     }
 
+    /**
+     * Executes the given interaction / SlashCommand
+     * @param {CommandInteraction} interaction 
+     */
     Execute(interaction) {
         console.log(`${this.commandName} executed`);
     }
