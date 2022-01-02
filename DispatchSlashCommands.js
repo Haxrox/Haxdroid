8a1c8e9a0ff4130feade12b180be29d0cd118d38
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-01-02 11:40:50 -0800

    removed DRY from dispatch command script

--- DispatchSlashCommands.js
+++ DispatchSlashCommands.js
@@ -1,4 +1,3 @@
-const {SlashCommandBuilder} = require("@discordjs/builders");
 const {REST} = require("@discordjs/rest");
 const {Routes} = require("discord-api-types/v9");
 const FileSystem = require('fs');
@@ -16,64 +15,31 @@ if (DISPATCH_COMMANDS) {
         const command = require(`./Commands/${file}`);
         commands.push(command.GetData());
     });
-} 
-/*
-else {
-    commands.push(
-        new SlashCommandBuilder().setName('ping').setDescription('Replies with the'),
-        new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-        new SlashCommandBuilder().setName('user').setDescription('Replies with user info!')
-    )
 }
-*/
 
-if (DISPATCH_GUILD_COMMANDS) {
-    rest.put(Routes.applicationGuildCommands(Configuration.CLIENT_ID, Configuration.GUILD_ID), {
-        body: DISPATCH_COMMANDS ? commands.map(command => command.toJSON()) : []
-    }).then((data) => {
-        console.log("Registered guild commands [%d]:", data.length);
-        data.forEach(command => {
-            console.log(`Registered %s [${command.id}]: %s`, command.name, command.description);
-        });
-    
-        if (DELETE_COMMANDS) {
-            const promises = [];
-            data.filter(command => true).forEach(command => {
-                console.log(`Deleting %s [${command.id}]: %s`, command.name, command.description);
-                const deleteUrl = `${Routes.applicationGuildCommands(Configuration.CLIENT_ID, Configuration.GUILD_ID)}/${command.id}`;
-                promises.push(rest.delete(deleteUrl));
-            });
-            return Promise.all(promises);
-        }
-    })
-    .catch(console.error)
-    .finally(() => {
-        console.log("Dispatched guild commands");
+var applicationCommands = DISPATCH_GUILD_COMMANDS ? Routes.applicationGuildCommands(Configuration.CLIENT_ID, Configuration.GUILD_ID) : Routes.applicationCommands(Configuration.CLIENT_ID);
+rest.put(applicationCommands, {
+    body: DISPATCH_COMMANDS ? commands.map(command => command.toJSON()) : []
+}).then((data) => {
+    console.log("Registered %scommands [%d]:", DISPATCH_GUILD_COMMANDS ? "guild " : "", data.length);
+    data.forEach(command => {
+        console.log(`Registered %s [${command.id}]: %s`, command.name, command.description);
     });
-} else {
-    rest.put(Routes.applicationCommands(Configuration.CLIENT_ID), {
-        body: DISPATCH_COMMANDS ? commands.map(command => command.toJSON()) : []
-    }).then((data) => {
-        console.log("Registered commands [%d]:", data.length);
-        data.forEach(command => {
-            console.log(`Registered %s [${command.id}]: %s`, command.name, command.description);
-        });
 
-        if (DELETE_COMMANDS) {
-            const promises = [];
-            data.filter(command => true).forEach(command => {
-                console.log(`Deleting %s [${command.id}]: %s`, command.name, command.description);
-                const deleteUrl = `${Routes.applicationGuildCommands(Configuration.CLIENT_ID)}/${command.id}`;
-                promises.push(rest.delete(deleteUrl));
-            });
-            return Promise.all(promises);
-        }
-    })
-    .catch(console.error)
-    .finally(() => {
-        console.log("Dispatched client commands");
-    });
-}
+    if (DELETE_COMMANDS) {
+        const promises = [];
+        data.filter(command => true).forEach(command => {
+            console.log(`Deleting %s [${command.id}]: %s`, command.name, command.description);
+            const deleteUrl = `${Routes.applicationGuildCommands(Configuration.CLIENT_ID, Configuration.GUILD_ID)}/${command.id}`;
+            promises.push(rest.delete(deleteUrl));
+        });
+        return Promise.all(promises);
+    }
+})
+.catch(console.error)
+.finally(() => {
+    console.log("Dispatched %scommands", DISPATCH_GUILD_COMMANDS ? "guild " : "");
+});
 
 
 
