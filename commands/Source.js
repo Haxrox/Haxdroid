15e19a3ba05d479325327f02d6bc0ba3977687c1
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-01-07 18:42:38 -0800

    added source command

---
+++ commands/Source.js
@@ -0,0 +1,10 @@
+const Command = require('./Command.js');
+const {MessageEmbed} = require('discord.js');
+
+class Source extends Command {
+    async Execute(interaction) {
+        interaction.reply("https://github.com/Haxrox/Haxdroid");
+    }
+}
+
+module.exports = new Source("Source", "Gets the bot source code");
