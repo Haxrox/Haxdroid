2a79f529d6dd11a1ec56bf765a62d9f7ac5adb07
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-01-13 19:14:49 -0800

    fixed variable declarations

--- commands/Info.js
+++ commands/Info.js
@@ -6,27 +6,26 @@ class Info extends Command {
     async Execute(interaction) {
         const embed = new MessageEmbed()
             // .setAuthor({name: interaction.client.user.username, iconURL: interaction.client.user.avatarURL()})
-            .setColor('#cacaca')
             .setTimestamp()
             .setFooter({text: `Requested by: ${interaction.user.username}`, iconURL: interaction.user.avatarURL()});
 			
         if (interaction.options.getSubcommand() === 'user') {
-            var user = interaction.options.getUser('target') || interaction.user;
+            const user = interaction.options.getUser('target') || interaction.user;
             
-            const color = user.hexAccentColor != null ? user.hexAccentColor : 'cacaca';
-            embed.setColor(color)
+            embed.setColor(user.hexAccentColor || '#cacaca')
             .setTitle(`${user.username} Info`)
             .setDescription(`**Profile:** ${user}`)
             .setThumbnail(user.avatarURL())
             .addField("Created", user.createdAt.toDateString(), true)
             .addField("Tag", user.tag, true);
 		} else if (interaction.options.getSubcommand() === 'server') {
-            var owner = await interaction.guild.fetchOwner({cache: true, force: true}) || "Undefined";
+            const owner = await interaction.guild.fetchOwner({cache: true, force: true}) || "Undefined";
             const description = interaction.guild.description;
             if (description != null) {
                 description = blockQuote(description);
                 embed.setDescription(description)
             }
+            
             embed.setTitle(`${interaction.guild.name} Info`)
             .setThumbnail(interaction.guild.iconURL())
             .addField("Created", interaction.guild.createdAt.toDateString(), true)
