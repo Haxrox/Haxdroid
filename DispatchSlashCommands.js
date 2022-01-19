2a83ec4ef0e732952a8a75a2d0cb7fa55d1d10c2
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-01-18 19:42:34 -0800

    fixed default dispatch script

--- DispatchSlashCommands.js
+++ DispatchSlashCommands.js
@@ -3,47 +3,35 @@ const {Routes} = require("discord-api-types/v9");
 const FileSystem = require('fs');
 const Configuration = require('./config.json');
 
-const DISPATCH_GUILD_COMMANDS = false;
-const DELETE_COMMANDS = false;
+const DISPATCH_GUILD_COMMANDS = true;
 const DISPATCH_COMMANDS = true;
 
 const rest = new REST({version: '9'}).setToken(Configuration.TESTING ? Configuration.TEST_TOKEN : Configuration.TOKEN);
 const commands = [];
 
-if (DISPATCH_COMMANDS) {
-    FileSystem.readdirSync('./commands').filter(file => (file.endsWith('.js') && file != 'Command.js' && file != 'SlashCommand.js')).forEach(file => {
-        const command = require(`./commands/${file}`);
-        const commandData = command.GetData()
-        .setDefaultPermission(!DELETE_COMMANDS);
-        commands.push(commandData);
-    });
-}
-
 const applicationCommands = DISPATCH_GUILD_COMMANDS ? Routes.applicationGuildCommands(Configuration.TESTING ? Configuration.TEST_CLIENT_ID : Configuration.CLIENT_ID, Configuration.GUILD_ID) : Routes.applicationCommands(Configuration.TESTING ? Configuration.TEST_CLIENT_ID : Configuration.CLIENT_ID);
 
-if (DELETE_COMMANDS) {
-    rest.get(applicationCommands).then(async data => {
-        console.log("Deleting %scommands [%d] ...", DISPATCH_GUILD_COMMANDS ? "guild " : "", data.length);
-        const promises = [];
-        data.filter(command => true).forEach(command => {
-            console.log(`Deleting %s [${command.id}]: %s`, command.name, command.description);
-            const deleteUrl = `${applicationCommands}/${command.id}`;
-            promises.push(rest.delete(deleteUrl));
+(async () => {
+    if (DISPATCH_COMMANDS) {
+        console.log("Dispatching %scommands ...", DISPATCH_GUILD_COMMANDS ? "guild " : "");
+    
+        const commandFiles = FileSystem.readdirSync('./commands').filter(file => (file.endsWith('.js') && file != 'Command.js' && file != 'SlashCommand.js'))
+        commandFiles.filter(command => true).forEach(file => {
+            const command = require(`./commands/${file}`);
+            const commandData = command.GetData()
+            .setDefaultPermission(command.defaultPermission);
+            commands.push(commandData.toJSON());
         });
-        return await Promise.all(promises);
-    })
-    .catch(console.error)
-    .finally(() => {
-        console.log("Deleted %scommands complete", DISPATCH_GUILD_COMMANDS ? "guild " : "");
-    });
-} else {
-    console.log("Dispatching commands ...");
-    rest.put(applicationCommands, {
-        body: DISPATCH_COMMANDS ? commands.map(command => command.toJSON()) : []
+    } else {
+        console.log("Deleting %scommands ...", DISPATCH_GUILD_COMMANDS ? "guild " : "");
+    }
+
+    await rest.put(applicationCommands, {
+        body: commands
     }).then((data) => {
         console.log("Registered %scommands [%d]:", DISPATCH_GUILD_COMMANDS ? "guild " : "", data.length);
         data.forEach(command => {
-            console.log(`Registered %s [${command.id}]: %s`, command.name, command.description);
+            console.log(`%s [${command.id}] [${command.default_permission}]: %s`, command.name, command.description);
         });
         return data;
     })
@@ -51,7 +39,4 @@ if (DELETE_COMMANDS) {
     .finally(() => {
         console.log("Dispatched %scommands complete", DISPATCH_GUILD_COMMANDS ? "guild " : "");
     });
-}
-
-
-
+})();
