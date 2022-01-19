db7a4799ce7203aeff573b374b8f9c2a9d05dc17
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-01-18 19:54:07 -0800

    implemented changing precense

--- events/Ready.js
+++ events/Ready.js
@@ -1,9 +1,17 @@
 const ClientEvent = require("./ClientEvent.js");
 
 class Ready extends ClientEvent {
+    CurrentPresence = 0;
     Execute(client) {
         super.Execute(client);
-        client.user.setPresence({activities: [{name: "Hello World!", type: "PLAYING"}], status: "online"});
+        setInterval(() => {
+            if (thisCurrentPrecense % 2 == 0) {
+                client.user.setPresence({activities: [{name: "Hello World!", type: "PLAYING"}], status: "online"});
+            } else if (this.CurrentPresence) {
+                client.user.setPresence({activities: [{name: `${new Date().toLocaleTimeString('en-US', { timeZone: 'America/Los_Angeles', hour12: true })} PST`, type: "PLAYING"}], status: "online"});
+            }
+            this.CurrentPrecense++;
+        }, 10000);
     }
 }
 
