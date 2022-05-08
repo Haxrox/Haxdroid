20830ccb2a35bd46217d4ae05d0d0cfce19b9abc
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-05-07 19:48:02 -0700

    committed constant file

---
+++ constants.js
@@ -0,0 +1,10 @@
+module.exports = Objects.freeze({
+    YOUTUBE_URL: "https://www.youtube.com/",
+    YOUTUBE_VIDEO_URL: "https://www.youtube.com/watch",
+    YOUTUBE_PLAYLIST_URL: "https://youtube.googleapis.com/youtube/v3/playlistItems",
+    YOUTUBE_SEARCH_REULTS_URL: "https://www.youtube.com/results",
+    YOUTUBE_SEARCH_URL: "https:/youtube.googleapis.com/youtube/v3/search",
+    YOUTUBE_MUSIC_SEARCH_RESULT_URL: "https://music.youtube.com/search",
+
+    GUILD_VOICE: 2
+})
