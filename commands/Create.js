b7d92d7c85835aea4aa7ed126017f71595173e96
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-05-27 12:10:30 -0700

    added sanity check to create command + made response an embed

--- commands/Create.js
+++ commands/Create.js
@@ -18,59 +18,69 @@ class Create extends Command {
         await interaction.deferReply();
         if (interaction.options.getSubcommand() == "channel") {
             if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS), true) {
-                var reason = interaction.options.getString("reason");
-                var categoryId = interaction.options.getString("category") || interaction.guild.systemChannel.parentId;
-                const channelType = interaction.options.getString("type", true);
-                const allowedData = interaction.options.getString("allow");
-                const denyData = interaction.options.getString("deny");
+                try {
+                    var reason = interaction.options.getString("reason");
+                    var categoryId = interaction.options.getString("category") || interaction.guild.systemChannel.parentId;
+                    const channelType = interaction.options.getString("type", true);
+                    const allowedData = interaction.options.getString("allow");
+                    const denyData = interaction.options.getString("deny");
 
-                reason = reason ? `for ${reason}` : "";
+                    reason = reason ? `for ${reason}` : "";
 
-                const permissions = [];
+                    const permissions = [];
 
-                allowedData?.split(" ").forEach(permission => {
-                    const matches = permission.matchAll(USERS_PATTERN).next().value || permission.matchAll(ROLES_PATTERN).next().value;
+                    allowedData?.split(" ").forEach(permission => {
+                        const matches = permission.matchAll(USERS_PATTERN).next().value || permission.matchAll(ROLES_PATTERN).next().value;
 
-                    if (matches) {
-                        permissions.push({
-                            id: matches[1],
-                            allow: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES]
-                        })
-                    }
-                });
-
-                denyData?.split(" ").forEach(permission => {
-                    console.log(permission);
-                    const matches = permission.matchAll(USERS_PATTERN).next().value || permission.matchAll(ROLES_PATTERN).next().value;
-
-                    if (matches) {
-                        permissions.push({
-                            id: matches[1],
-                            deny: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES]
-                        })
-                    } else if (permission.includes("everyone")) {
-                        interaction.guild.roles.cache.every(role => {
+                        if (matches) {
                             permissions.push({
-                                id: role.id,
+                                id: matches[1],
+                                allow: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES]
+                            })
+                        }
+                    });
+
+                    denyData?.split(" ").forEach(permission => {
+                        console.log(permission);
+                        const matches = permission.matchAll(USERS_PATTERN).next().value || permission.matchAll(ROLES_PATTERN).next().value;
+
+                        if (matches) {
+                            permissions.push({
+                                id: matches[1],
                                 deny: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES]
                             })
-                        })
+                        } else if (permission.includes("everyone")) {
+                            interaction.guild.roles.cache.every(role => {
+                                permissions.push({
+                                    id: role.id,
+                                    deny: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES]
+                                })
+                            })
+                        }
+                    })
+
+                    const channelOptions = {
+                        type: channelType,
+                        permissionOverwrites: permissions,
+                        reason: `created by ${interaction.client.user.username} ${reason}`,
+                    };
+
+                    if (channelType == GUILD_CATEGORY) {
+                        var channel = await this.CreateCategory(interaction.guild.channels, interaction.options.getString("name", true), channelOptions);
+                    } else {
+                        var channel = await this.CreateChannel(interaction.options.getString("name", true), channelOptions, await interaction.guild.channels.fetch(categoryId));
                     }
-                })
 
-                const channelOptions = {
-                    type: channelType,
-                    permissionOverwrites: permissions,
-                    reason: `created by ${interaction.client.user.username} ${reason}`,
-                };
-
-                if (channelType == GUILD_CATEGORY) {
-                    var channel = await this.CreateCategory(interaction.guild.channels, interaction.options.getString("name", true), channelOptions);
-                } else {
-                    var channel = await this.CreateChannel(interaction.options.getString("name", true), channelOptions, await interaction.guild.channels.fetch(categoryId));
+                    const embed = new MessageEmbed()
+                        .setTitle(`Channel created`)
+                        .setDescription(channelType === GUILD_CATEGORY ? `<${channel}>` : channel)
+                        .setColor(Styles.Colours.Theme)
+                        .setTimestamp()
+                        .setFooter({ text: `Created by: ${interaction.user.username}`, iconURL: interaction.user.avatarURL() });
+                    interaction.editReply({ embeds: [embed] });
+                } catch (error) {
+                    this.DeferError(interaction, error.message);
                 }
-
-                interaction.editReply(`Created channel: ${channelType === GUILD_CATEGORY ? `<${channel}>` : channel}`);
             } else {
                 this.DeferError(interaction, "Not enough permissions");
             }
