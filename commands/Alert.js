235443e84e398a20e4b045eb70fad5d3fe706dad
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-06-19 14:56:13 -0700

    renamed spam command to alert

--- commands/Alert.js
+++ commands/Alert.js
@@ -4,55 +4,59 @@ const Command = require('./Command.js');
 const { MessageEmbed } = require('discord.js');
 const { blockQuote, bold } = require('@discordjs/builders');
 
-const SPAM_RATE = 1000; // rate to send each message
-const MAX_SPAM = 25; // number of messages
-const MAX_LENGTH = 60; // in seconds
+const ALERT_RATE = 500; // rate to send each message (ms / message)
+const MAX_ALERT = 25; // number of messages
+const MAX_LENGTH = 300; // in seconds
 
-function spam(spammer, target, message, duration, count) {
-    const spamEmbed = new MessageEmbed()
+function alert(alerter, target, message, duration, count) {
+    const alertEmbed = new MessageEmbed()
         .setTitle(message)
         .setColor(Styles.Colours.Theme)
-        .setFooter({ text: `Spammed by: ${spammer.username}`, iconURL: spammer.avatarURL() });
+        .setFooter({ text: `Alerted by: ${alerter.username}`, iconURL: alerter.avatarURL() });
 
     const resultEmbed = new MessageEmbed()
-        .setTitle("Spam Complete")
+        .setTitle("Alert Complete")
         .setColor(Styles.Colours.Green)
 
-    count = count ? Math.min(count, MAX_SPAM) : 0;
+    count = count ? Math.min(count, MAX_ALERT) : 0;
 
     const id = setInterval(() => {
-        target.send({ embeds: [spamEmbed.setTimestamp()] }).then(() => {
+        target.send({ embeds: [alertEmbed.setTimestamp()] }).then(() => {
             count--;
             if (!duration && count <= 0) {
                 clearInterval(id);
-                spammer.send({ embeds: [resultEmbed] });
+                alerter.send({ embeds: [resultEmbed] });
             }
         }).catch(error => {
             clearInterval(id);
-            spammer.send({
+            alerter.send({
                 embeds: [
                     resultEmbed
-                        .setTitle("Spam Failed")
+                        .setTitle("Alert Failed")
                         .setDescription(`Cannot send messages to ${target}\n${blockQuote(error)}`)
                         .setTimestamp()
                 ]
             })
         });
-    }, SPAM_RATE);
+    }, ALERT_RATE);
 
     if (duration) {
         duration = Math.min(duration, MAX_LENGTH);
         setTimeout(() => {
             clearInterval(id);
-            spammer.send({ embeds: [resultEmbed.setTimestamp()] });
+            alerter.send({ embeds: [resultEmbed.setTimestamp()] });
         }, duration * 1000);
     }
 }
 
 /**
- * Spam command - SHOULD NOT BE ABUSED. I wrote this to help wake people up for valorant games
+ * Alert command - SHOULD NOT BE ABUSED. I wrote this to help wake people up for valorant games
  */
-class Spam extends Command {
+class Alert extends Command {
+    Validate(member) {
+        return member.user.id === Config.OWNER_ID;
+    }
+
     async Execute(interaction) {
         if (interaction.user.id === Config.OWNER_ID) {
             const message = interaction.options.getString("message", true);
@@ -61,37 +65,37 @@ class Spam extends Command {
             const count = interaction.options.getInteger("count");
 
             if (duration != null || count != null) {
-                spam(interaction.user, target, message, duration, count);
+                alert(interaction.user, target, message, duration, count);
 
                 const embed = new MessageEmbed()
-                    .setTitle("Spam Initialized")
-                    .setDescription(`Spamming ${target} ${duration ? `for ${bold(Math.min(duration, MAX_LENGTH))} seconds` : `${bold(Math.min(count, MAX_SPAM))} times`} with message:\n${blockQuote(message)}`)
+                    .setTitle("Alert Initialized")
+                    .setDescription(`Alerting ${target} ${duration ? `for ${bold(Math.min(duration, MAX_LENGTH))} seconds` : `${bold(Math.min(count, MAX_ALERT))} times`} with message:\n${blockQuote(message)}`)
                     .setColor(Styles.Colours.Theme)
                     .setTimestamp()
-                    .setFooter({ text: `Spammed by: ${interaction.user.username}`, iconURL: interaction.user.avatarURL() });
+                    .setFooter({ text: `Alerted by: ${interaction.user.username}`, iconURL: interaction.user.avatarURL() });
 
                 interaction.reply({ embeds: [embed] });
             } else {
                 this.Error(interaction, "Field `duration` or `count` must be filled");
             }
         } else {
-            this.Error(interaction, "Cannot user this command");
+            this.Error(interaction, "Cannot use this command");
         }
     }
 }
 
-const SpamCommand = new Spam("Spam", "Repeatedly sends messages to a certain user");
-SpamCommand.GetData()
+const AlertCommand = new Alert("Alert", "Repeatedly sends messages to a certain user");
+AlertCommand.GetData()
     .addUserOption(option =>
-        option.setName("target").setDescription("user to spam").setRequired(true)
+        option.setName("target").setDescription("user to alert").setRequired(true)
     )
     .addStringOption(option =>
-        option.setName("message").setDescription("Message to spam").setRequired(true)
+        option.setName("message").setDescription("message to alert").setRequired(true)
     )
     .addIntegerOption(option =>
-        option.setName("duration").setDescription("length to spam the messages")
+        option.setName("duration").setDescription("length to alert the messages")
     )
     .addIntegerOption(option =>
         option.setName("count").setDescription("number of messages to send")
     )
-module.exports = SpamCommand;
+module.exports = AlertCommand;
