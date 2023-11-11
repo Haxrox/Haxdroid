94a57c0e7ef95d81c9adf383866fc297ba5f9ab4
Haxrox <56497846+Haxrox@users.noreply.github.com>
2023-11-11 13:17:53 -0500

    resolves #19: improved CI
    Updated npm scripts to use arguments to reduce code duplication
    Modified GitHub workflow to pass arguments

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -4,14 +4,14 @@
 # Decrypts secret JSON file using gpg
 # Arguments: 
 #   $1: file that holds the encrypted secrets
-#   $2: passphrase used to encrypt the secrets
-#   $3: path to store decrypted secrets
+#   $2: path to store decrypted secrets
+#   $3: passphrase used to encrypt the secrets
 # Returns:
 #   None
 function main() {
   local secret_file_path="$1"
-  local passphrase="$2"
-  local result_file_path="$3"
+  local result_file_path="$2"
+  local passphrase="$3"
 
   gpg --quiet --batch --yes --decrypt --passphrase="${passphrase}" --output "${result_file_path}" "${secret_file_path}"
 }
