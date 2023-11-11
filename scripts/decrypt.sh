bc75b520455c853c99aa658a92dad34df6e2301c
Haxrox <56497846+Haxrox@users.noreply.github.com>
2023-11-11 14:41:45 -0500

    resolves #23: added CD secrets
    Improved decrypt.sh to decrypt all forms of secrets
    Added pm2 deployment

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+# decrypt.sh
+# Decrypts secrets used in this repository
+# Arguments:
+#   $1: passphrase used to encrypt the secrets
 
 ########################################
 # Decrypts secret JSON file using gpg
@@ -8,7 +12,7 @@
 #   $3: passphrase used to encrypt the secrets
 # Returns:
 #   None
-function main() {
+function decrypt() {
   local secret_file_path="$1"
   local result_file_path="$2"
   local passphrase="$3"
@@ -16,4 +20,22 @@ function main() {
   gpg --quiet --batch --yes --decrypt --passphrase="${passphrase}" --output "${result_file_path}" "${secret_file_path}"
 }
 
+########################################
+# Main function
+# Arguments: 
+#   $1: passphrase used to encrypt the secrets
+# Returns:
+#   None
+function main() {
+  local passphrase="$1"
+
+  # Decrypt key file
+  decrypt "../HaxVM_key.pem.gpg" "../HaxVM_key.pem" passphrase
+  chmod 400 "../HaxVM_key.pem"
+  # Decrypt config file
+  decrypt "../configs/config.json.gpg" "../configs/config.json" passphrase
+
+  # Decrypt ecosystem.config.js
+  decrypt "../configs/ecosystem.config.js.gpg" "../configs/ecosystem.config.js" passphrase
+}
 main "$@"
