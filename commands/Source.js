396b67df7e29b4fae5b1bc5201ebd217fde73b09
Haxrox <56497846+Haxrox@users.noreply.github.com>
2023-11-21 22:04:52 -0500

    resolves #38: add git info to source command
    Uses Git service to send commands to bash shell to fetch git repo
    information

--- commands/Source.js
+++ commands/Source.js
@@ -1,4 +1,8 @@
+const {EmbedBuilder, hyperlink} = require('discord.js');
+
 const Command = require('./Command.js');
+const Git = require('../services/Git.js');
+const Styles = require('../styles.json');
 
 /**
  * Returns GitHub repo for bot
@@ -9,7 +13,41 @@ class Source extends Command {
    * @param {BaseInteraction} interaction interaction executed
    */
   async execute(interaction) {
-    interaction.reply('https://github.com/Haxrox/Haxdroid');
+    await interaction.deferReply();
+
+    const git = new Git();
+
+    Promise.allSettled([
+      git.getBranch(),
+      git.getRevision(),
+      git.getRemote(),
+    ]).then((results) => {
+      const branch = results[0].value;
+      const revision = results[1].value;
+      const remote = results[2].value;
+
+      const embed = new EmbedBuilder()
+          .setTitle(`${interaction.client.user.username} Source`)
+          .setURL(remote)
+          .setThumbnail(interaction.client.user.avatarURL())
+          .addFields([
+            {
+              name: 'Branch',
+              value: hyperlink(branch, remote.concat('/tree/', branch)),
+            },
+            {
+              name: 'Revision',
+              value: hyperlink(revision, remote.concat('/commit/', revision)),
+            },
+          ])
+          .setColor(Styles.Colours.Theme)
+          .setTimestamp()
+          .setFooter({
+            text: `Requested by: ${interaction.user.username}`,
+            iconURL: interaction.user.avatarURL(),
+          });
+      interaction.editReply({embeds: [embed]});
+    });
   }
 }
 
